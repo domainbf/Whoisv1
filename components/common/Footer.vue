dd858b64ac1a224ec82e9d44e3c6579013794e80
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-01-28 20:00:29 +0800

    Update Footer.vue

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-const localePath = useLocalePath()
-const {t} = useI18n()
-</script>
 <template>
   <footer class="text-gray-800 h-[10vh] bg-[#F1F3F4] dark:text-white  dark:bg-[#5b77af]">
     <div class="max-w-5xl mx-auto py-4 px-4 flex justify-between items-center">
@@ -10,13 +6,11 @@ const {t} = useI18n()
       </div>
       <div class="flex items-center space-x-4">
         <NuxtLink :to="localePath('/api.html')" class="hover:underline">{{ t('footer.api') }}</NuxtLink>
-        <NuxtLink to="https://github.com/7836246/Nuxt-Whois" class="hover:underline">
-          <Icon name="ant-design:github-outlined" class="h-6 w-6" />
+        <NuxtLink to="https://whois.ls" class="hover:underline">
+          <!-- 替换为字母 W 的图标 -->
+          <span class="text-2xl font-bold">W</span>
         </NuxtLink>
       </div>
     </div>
   </footer>
 </template>
-
-<style scoped>
-</style>
