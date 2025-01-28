3a8403d11347865d92e6e7449070ec7f9777838d
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-01-28 20:11:21 +0800

    Update Footer.vue

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,22 +1,21 @@
-<script setup lang="ts">
-const localePath = useLocalePath()
-const { t } = useI18n()
-</script>
 <template>
   <footer class="text-gray-800 h-[10vh] bg-[#F1F3F4] dark:text-white dark:bg-[#5b77af]">
     <div class="max-w-5xl mx-auto py-4 px-4 flex justify-between items-center">
       <div class="text-sm">
-        {{ t('footer.text') }}
+        {{ t('footer.text')}}
       </div>
       <div class="flex items-center space-x-4">
         <NuxtLink :to="localePath('/api.html')" class="hover:underline">{{ t('footer.api') }}</NuxtLink>
-        <NuxtLink to="https://whois.ls" class="hover:underline">
-          <Icon name="fab:globe" class="h-6 w-6" />
+        <NuxtLink to="https://github.com/7836246/Nuxt-Whois" class="hover:underline">
+          <GlobalOutlined />
         </NuxtLink>
       </div>
     </div>
   </footer>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { GlobalOutlined } from '@ant-design/icons-vue';
+const localePath = useLocalePath()
+const { t } = useI18n()
+</script>
