797206dbb9f6bd8fc18e7ccb5e6df28d0acd4689
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-01-28 20:41:03 +0800

    Update Footer.vue

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' // You can remove this import if not used elsewhere
-import { faGlobe } from '@fortawesome/free-solid-svg-icons' // You can remove this import if not used elsewhere
-import { library } from '@fortawesome/fontawesome-svg-core' // You can remove this import if not used elsewhere
-
-library.add(faGlobe) // You can remove this line if not used elsewhere
-
 const localePath = useLocalePath()
 const { t } = useI18n()
 </script>
-
 <template>
   <footer class="text-gray-800 h-[10vh] bg-[#F1F3F4] dark:text-white dark:bg-[#5b77af]">
     <div class="max-w-5xl mx-auto py-4 px-4 flex justify-between items-center">
@@ -17,7 +10,10 @@ const { t } = useI18n()
       </div>
       <div class="flex items-center space-x-4">
         <NuxtLink :to="localePath('/api.html')" class="hover:underline">{{ t('footer.api') }}</NuxtLink>
-        </div>
+        <NuxtLink to="https://whois.ls" class="hover:underline">
+          <Icon name="域名报价" class="h-6 w-6" />
+        </NuxtLink>
+      </div>
     </div>
   </footer>
 </template>
