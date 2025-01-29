71e40d02cefa46bb2d408470a04897f0d78bcf05
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-01-30 06:14:33 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Nuxt Whois' }
+            { hid: 'description', name: 'description', content: 'whois.ls' }
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
