31ac84cc8727d1a6b9980874d2f30eab65bba40d
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-01-30 06:49:16 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,33 +10,41 @@ export default defineNuxtConfig({
     '@pinia-plugin-persistedstate/nuxt',
     'nuxt-simple-robots',
   ],
-  features:{
-      inlineStyles: true,
-  } ,
-  runtimeConfig: {
-      public: {
-          Domain: 'Domain+DNS Lookup',
-          DomainSuffix: 'whois.ls',
-      }
+  features: {
+    inlineStyles: true,
   },
-  app:{
+  runtimeConfig: {
+    public: {
+      Domain: 'Domain+DNS Lookup',
+      DomainSuffix: 'whois.ls',
+    }
+  },
+  app: {
     head: {
-        title: 'Whois.ls',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'whois.ls' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
+      title: 'Whois.ls',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'whois.ls' },
+        { hid: 'og:title', property: 'og:title', content: 'Whois.ls' },
+        { hid: 'og:description', property: 'og:description', content: 'whois.ls - Domain and DNS Lookup' },
+        { hid: 'og:image', property: 'og:image', content: '/whoisls.png' },
+        { hid: 'og:url', property: 'og:url', content: 'https://whois.ls' },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:title', name: 'twitter:title', content: 'Whois.ls' },
+        { hid: 'twitter:description', name: 'twitter:description', content: 'whois.ls - Domain and DNS Lookup' },
+        { hid: 'twitter:image', name: 'twitter:image', content: '/whois.ls.png' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
     }
   },
   i18n: {
     strategy: 'prefix_except_default',
     defaultLocale: 'zh',
     detectBrowserLanguage: {
-          useCookie: true,
+      useCookie: true,
     },
     locales: [
       { code: 'zh', iso: 'zh-Hans', file: 'zh.ts' },
@@ -48,4 +56,4 @@ export default defineNuxtConfig({
   headlessui: {
     prefix: 'Headless'
   },
-})
+});
