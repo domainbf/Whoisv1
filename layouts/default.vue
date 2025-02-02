0f2e8a7b30907857d21874f46702f28d91f4226d
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-02-03 00:26:18 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -80,62 +80,65 @@ onMounted(() => {
 const showToast = (message: string) => {
   toast.add({
     title: message,
-    position: "center", // 将提示信息放在屏幕中间
-    duration: 3000, // 设置提示信息的显示时间（可选）
+    position: "center",
+    duration: 3000,
   });
 };
 </script>
 
 <template>
-  <div
-    class="w-full text-xs bg-[#F1F3F4] dark:bg-transparent"
-    :class="{ 'h-[90vh]': !styleStore.getIsPage && clientMounted }"
-  >
-    <div
-      class="max-w-screen-lg mx-auto px-[1em] pb-[10vh]"
-      :class="{ 'pt-[25vh]': !styleStore.getIsPage && clientMounted, 'pt-[5vh]': styleStore.getIsPage || !clientMounted }"
-    >
-      <nav class="w-full text-[#464747] h-5 dark:bg-gray-700">
-        <NuxtLink
-          :to="localePath('/')"
-          class="mb-3 font-bold text-2xl inline-block text-current no-underline dark:text-white"
+  <div class="flex flex-col min-h-screen">
+    <div class="flex-grow">
+      <div
+        class="w-full text-xs bg-[#F1F3F4] dark:bg-transparent"
+        :class="{ 'h-[90vh]': !styleStore.getIsPage && clientMounted }"
+      >
+        <div
+          class="max-w-screen-lg mx-auto px-[1em] pb-[10vh]"
+          :class="{ 'pt-[25vh]': !styleStore.getIsPage && clientMounted, 'pt-[5vh]': styleStore.getIsPage || !clientMounted }"
         >
-          <h1 class="inline-block text-current no-underline dark:text-white">{{ runtimeConfig?.public?.Domain }}</h1>
-          <sup class="text-[#59a8d7] dark:text-[#ace4f8]">{{ runtimeConfig?.public?.DomainSuffix }}</sup>
-        </NuxtLink>
-      </nav>
+          <nav class="w-full text-[#464747] h-5 dark:bg-gray-700">
+            <NuxtLink
+              :to="localePath('/')"
+              class="mb-3 font-bold text-2xl inline-block text-current no-underline dark:text-white"
+            >
+              <h1 class="inline-block text-current no-underline dark:text-white">{{ runtimeConfig?.public?.Domain }}</h1>
+              <sup class="text-[#59a8d7] dark:text-[#ace4f8]">{{ runtimeConfig?.public?.DomainSuffix }}</sup>
+            </NuxtLink>
+          </nav>
 
-      <div class="mt-6">
-        <UForm
-          :state="state"
-          class="flex items-center space-x-2 mb-3 dark:text-white"
-          @submit="handleAction('whois')"
-        >
-          <div class="flex-grow">
-            <UInput
-              v-model="state.domain"
-              :placeholder="t('index.placeholder')"
-              color="sky"
-              size="xl"
-              class="w-full"
-            />
+          <div class="mt-6">
+            <UForm
+              :state="state"
+              class="flex items-center space-x-2 mb-3 dark:text-white"
+              @submit="handleAction('whois')"
+            >
+              <div class="flex-grow">
+                <UInput
+                  v-model="state.domain"
+                  :placeholder="t('index.placeholder')"
+                  color="sky"
+                  size="xl"
+                  class="w-full"
+                />
+              </div>
+              <UButton type="submit" color="sky" size="xl" v-if="state.domain">
+                {{ t('index.onSubmit') }}
+              </UButton>
+            </UForm>
           </div>
-          <UButton type="submit" color="sky" size="xl" v-if="state.domain">
-            {{ t('index.onSubmit') }}
-          </UButton>
-        </UForm>
-      </div>
-      <CommonBulletin 
-  v-if="!styleStore.isPage && clientMounted"
-/>
+          <CommonBulletin 
+            v-if="!styleStore.isPage && clientMounted"
+          />
 
-      <TabList @action="handleAction" />
-      <slot />
+          <TabList @action="handleAction" />
+          <slot />
+        </div>
+      </div>
     </div>
+    <CommonFooter />
   </div>
-  <CommonFooter />
 </template>
 
 <style scoped>
-
 </style>
